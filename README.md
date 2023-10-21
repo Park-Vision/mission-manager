8208d58d3e4fc70b66edbe625c0f9d78995e9066
Szymon Romanek <srszymonsr@gmail.com>
2023-10-21 23:39:24 +0200

    startup instructions

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Service which manages drone flight path and communication with the main system.
+
+# First run - local
+```poetry install```
+```poetry run python mission-manager```
+
+With kafka for linux- works on my machine - kafka needs to be installed in a certain directory etc. etc.
+```sudo run_locally.sh```
---
+++ run_locally.sh
@@ -0,0 +1,6 @@
+# Run ardupilot SITL in docker + zookeeper + kafka
+
+gnome-terminal -e "docker run -it --net=host --env="DISPLAY" --volume="/run/user/1000/.mutter-Xwaylandauth.51D0B2:/home/akl/.Xauthority:rw" wnt3rmute/ardupilot-sitl ./sim_vehicle.py -v ArduCopter -N"
+gnome-terminal -e "sudo /opt/kafka_2.13-3.6.0/bin/zookeeper-server-start.sh /opt/kafka_2.13-3.6.0/config/zookeeper.properties"
+gnome-terminal -e "sudo /opt/kafka_2.13-3.6.0/bin/kafka-server-start.sh /opt/kafka_2.13-3.6.0/config/server.properties"
+gnome-terminal -e "/opt/kafka_2.13-3.6.0/bin/kafka-console-consumer.sh --topic quickstart-events --from-beginning --bootstrap-server localhost:9092"
