e86b620f237f7f664e617666577d2887e95a6a01
Szymon Romanek <srszymonsr@gmail.com>
2023-11-19 09:48:00 +0100

    Tell them to bring out more test cases

--- manager/test/test_path.py
+++ manager/test/test_path.py
@@ -1,10 +1,14 @@
+from random import shuffle
+
+import pytest
+
 from manager.mission.path import create_path
 from manager.mission.waypoint import Waypoint
 
 LANDING_POINT = (51.11016, 17.05963)
 
 
-correct_order = [
+correct_order_1 = [
     LANDING_POINT,
     (51.11016, 17.05936),
     (51.11002, 17.0591),
@@ -14,24 +18,29 @@ correct_order = [
     LANDING_POINT,
 ]
 
-points = [
+correct_order_2 = [
     LANDING_POINT,
-    (51.11016, 17.05936),
-    (51.10977, 17.05894),
-    (51.10987, 17.05903),
-    (51.10996, 17.05923),
-    (51.11002, 17.0591),
+    (51.110965793535975, 16.87588381999903),
+    (51.11194843653306, 16.87352422484061),
+    (51.112464439189026, 16.871782057081067),
+    (51.11273231388913, 16.870989626376424),
+    LANDING_POINT,
 ]
 
-
-def test_path():
-    points_waypoints = [Waypoint(0, point[0], point[1]) for point in points]
+@pytest.mark.parametrize('correct_order', [correct_order_1, correct_order_2])
+def test_path(correct_order):
     correct_order_waypoints = [
         Waypoint(0, point[0], point[1]) for point in correct_order
     ]
+    
+    # Randomize order
+    input_points = [p for p in correct_order if p != LANDING_POINT]
+    shuffle(input_points)
+    # Landing point needs to be first, otherwise
+    # path could start from the opposite side
+    input_points.insert(0, LANDING_POINT)
 
-    path = create_path(points_waypoints)
+    input_waypoints = [Waypoint(0, point[0], point[1]) for point in input_points]
+
+    path = create_path(input_waypoints)
     assert path == correct_order_waypoints
-
-
-test_path()
